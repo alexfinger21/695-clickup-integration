7e5708a660fb00a0d378a02291869d930e70b893
SuperMeerkat345 <artiom22903@gmail.com>
2024-07-18 19:26:35 -0400

    added dummy user login

--- app.py
+++ app.py
@@ -2,18 +2,41 @@ def acceptInput():
     command = input()
     match command:
         case "help":
-            print("Commands:\ndisplay")
+            print("Commands:\ndisplay\exit")
             return True
         case "display":
             print("Your tasks are")
             return True
         case "exit":
+            print("Signing out of clickup")
             return False
         case _:
             print("Command not recognized")
             return True
 
+# Dummy login 
+def getUser(user, pw):
+    match user:
+        case "admin":
+            if pw == "rev":
+                return True
+            else:
+                print("Invalid Password")
+                return False
+        case "user":
+            if pw == "pw":
+                return True
+            else: 
+                print("Invalid password")
+                return False
+        case _:
+            print("Invalid username")
+
 username = input("Username: ")
+pw = input("Password: ")
+
+getUser(username, pw)
+
 print('Enter "help" to view commands')
 while acceptInput():
     print("...")
