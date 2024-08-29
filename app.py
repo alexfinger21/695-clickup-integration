9516040526e748d00cc3d19f85d8ec9d5e879cc3
SuperMeerkat345 <artiom22903@gmail.com>
2024-08-29 18:47:01 -0400

    .venv

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
+# dummy db
 users = {
     "admin": "rev",
     "jerry": "123",
     "gerald": "screw-jerry"
 }
 
+
+if 'G_member' in locals():
+    print("Signed in as" + G_member + ", if you need help type 'help'")
+else:
+    print("Signed in as *, if you need help type 'help'")
+
+# process all input in the cli
 def acceptInput():
     command = input()
     match command:
         case "help":
-            print("Commands:\ndisplay\exit")
+            print("Commands:\nmytasks - Display current assigned tasks\nsign out - Sign out of clickup")
             return True
-        case "display":
+        case "mytasks":
             print("Your tasks are")
             return True
-        case "exit":
+        case "sign out":
             print("Signing out of clickup")
             return False
         case _:
@@ -48,8 +56,14 @@ def loggedIn():
     while acceptInput():
         print("...")
 
+var loginB = input("Login to ClickUp? (y/n)")
 
-username = input("Username: ")
-pw = input("Password: ")
+if loginB == "y" or loginB == "yes" or loginB == "Y" or loginB == "Yes" :
+    username = input("Username: ")
+    pw = input("Password: ")
 
-getUser(username, pw)
+    getUser(username, pw)
+else: 
+    print("Not logged in")
+
+
