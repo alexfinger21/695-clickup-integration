2793bbe17fa0cb5c9fbfc22332abc85cf7f22f31
alexfinger21 <cdx9933@gmai1.com>
2024-09-21 11:05:27 -0400

    ok

--- api/funcs.py
+++ api/funcs.py
@@ -1,30 +1,35 @@
 import json
-from urllib.request import Request, urlopen
+from requests import Request, Session
 import warnings
+
+s = Session() # speeds up the requests
 
 def display_tasks(emails: set, statuses: set) -> str:
     if(len(emails) == 0):
         warnings.warn("User has empty email")
         return
+
     display_url = f"https://api.clickup.com/api/v2/list/901101497371/task?{'&'.join(['statuses[]=' + x.replace(' ', '+') for x in statuses])}&page=0"
+
     
     print(display_url)
 
     apiFINDReq = Request(
+        "GET",
         display_url, 
         data=json.dumps({"test": True}).encode(), 
-        method="GET",
         headers={"Authorization": "pk_14742653_2DD8K5CYR2LMRSZ7TWVE8POG2FB0EVKB"}
     ) #test if login required works
+    apiFINDReq = apiFINDReq.prepare()
 
-    tasks = urlopen(apiFINDReq)
-    tasks = json.loads(tasks.read())["tasks"]
-    print(tasks[0])
+    res = s.send(apiFINDReq, 
+         stream={}, 
+         verify=True, 
+         proxies=None, 
+         cert=None, 
+         timeout=None
+    )
 
-    res = []
+    res = json.loads(res.text)["tasks"]
+    return [x.get("name") for x in res if [z["email"] for z in x["assignees"] if z["email"] in emails]]
 
-    for x in res:
-        for a in x["assignees"]:
-            if(emails.has(a)):
-                res.append(x.get("name"))
-    print(res)
